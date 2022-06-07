d6e388706fc41eb9db91af535670cd362d9a0652
root <willtriti03@naver.com>
2022-06-07 14:42:10 +0000

    server

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const express = require("express");
+//express 사용
+const app = express(); 
+
+/**
+ * @path http://localhost:3000/ 경로
+ */
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+  
+// http listen port 생성 서버 실행
+app.listen(8080, () => console.log("개발이 취미인 남자 :)"));
